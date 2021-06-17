aeec924d25b680303cd984bb0a04e8a4ca703e34
Ayco Holleman <ayco.holleman@naturalis.nl>
2021-06-17 12:07:45 +0200

    nieuwe maven repo url

--- src/main/java/nl/naturalis/yokete/db/SQLQuery.java
+++ src/main/java/nl/naturalis/yokete/db/SQLQuery.java
@@ -4,12 +4,16 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 import nl.naturalis.common.ExceptionMethods;
 import nl.naturalis.common.check.Check;
+import nl.naturalis.yokete.db.rs.Emitter;
+import nl.naturalis.yokete.db.rs.EmitterNegotiator;
 import static nl.naturalis.common.ObjectMethods.ifNull;
 
 public class SQLQuery extends SQLStatement<SQLQuery> {
@@ -36,19 +40,47 @@ public class SQLQuery extends SQLStatement<SQLQuery> {
     }
   }
 
-  public ResultSet executeAndNext() {
+  public ResultSet executeAndNext() throws KSQLException {
     try {
       ResultSet rs = resultSet();
       if (!rs.next()) {
         throw new KSQLException("Query returned zero rows");
       }
       return rs;
+    } catch (KSQLException e0) {
+      throw e0;
     } catch (Throwable t) {
       throw ExceptionMethods.uncheck(t);
     }
   }
 
-  public int getInt() {
+  /**
+   * Returns the value of the first column of the first row, converted to the specified type. Throws
+   * a {@link KSQLException} if the query returned zero rows.
+   *
+   * @param <T>
+   * @param clazz
+   * @return
+   */
+  public <T> T lookup(Class<T> clazz) {
+    ResultSet rs = executeAndNext();
+    try {
+      int sqlType = rs.getMetaData().getColumnType(1);
+      Emitter<?, T> emitter = EmitterNegotiator.getInstance().getEmitter(clazz, sqlType);
+      return emitter.getValue(rs, 1, clazz);
+    } catch (Throwable t) {
+      throw ExceptionMethods.uncheck(t);
+    }
+  }
+
+  /**
+   * Returns the value of the first column of the first row as an integer. Throws a {@link
+   * KSQLException} if the query returned zero rows.
+   *
+   * @return The value of the first column of the first row as an integer
+   * @throws KSQLException If the query returned zero rows
+   */
+  public int getInt() throws KSQLException {
     try {
       return executeAndNext().getInt(1);
     } catch (SQLException e) {
@@ -56,11 +88,33 @@ public class SQLQuery extends SQLStatement<SQLQuery> {
     }
   }
 
-  public String getString() {
+  public String getString() throws KSQLException {
     try {
       return executeAndNext().getString(1);
     } catch (SQLException e) {
       throw ExceptionMethods.uncheck(e);
+    }
+  }
+
+  public <T> List<T> getList(Class<T> clazz) {
+    return getList(clazz, 10);
+  }
+
+  public <T> List<T> getList(Class<T> clazz, int expectedSize) {
+    try {
+      ResultSet rs = resultSet();
+      if (!rs.next()) {
+        return Collections.emptyList();
+      }
+      int sqlType = rs.getMetaData().getColumnType(1);
+      Emitter<?, T> emitter = EmitterNegotiator.getInstance().getEmitter(clazz, sqlType);
+      List<T> list = new ArrayList<>(expectedSize);
+      do {
+        list.add(emitter.getValue(rs, 1, clazz));
+      } while (rs.next());
+      return list;
+    } catch (Throwable t) {
+      throw ExceptionMethods.uncheck(t);
     }
   }
 
@@ -77,6 +131,15 @@ public class SQLQuery extends SQLStatement<SQLQuery> {
     try {
       MappifierBox mb = sql.getMappifierBox(nameMapper);
       return mb.get(resultSet()).mappifyAtMost(limit);
+    } catch (Throwable t) {
+      throw ExceptionMethods.uncheck(t);
+    }
+  }
+
+  public List<Row> mappifyAll() {
+    try {
+      MappifierBox mb = sql.getMappifierBox(nameMapper);
+      return mb.get(resultSet()).mappifyAll();
     } catch (Throwable t) {
       throw ExceptionMethods.uncheck(t);
     }
