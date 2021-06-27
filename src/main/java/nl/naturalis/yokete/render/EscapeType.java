3ba9db2fbbc27543045b76e212ea6a5edec58506
Ayco Holleman <ayco.holleman@naturalis.nl>
2021-06-27 08:45:09 +0200

    added more escape types

--- src/main/java/nl/naturalis/yokete/render/EscapeType.java
+++ src/main/java/nl/naturalis/yokete/render/EscapeType.java
@@ -2,6 +2,8 @@ package nl.naturalis.yokete.render;
 
 import java.util.function.UnaryOperator;
 import org.apache.commons.text.StringEscapeUtils;
+import org.apache.http.client.utils.URIBuilder;
+import nl.naturalis.common.check.Check;
 import static nl.naturalis.common.StringMethods.*;
 
 /**
@@ -15,15 +17,11 @@ public enum EscapeType {
    * The escape type assigned to template variables that don't specify an inline escape type. For
    * example, the escape type of {@code ~%html:person.address.street%} is {@code ESCAPE_HTML}
    * whereas the escape type of {@code ~%person.address.street%} is {@code NOT_SPECIFIED}. It is not
-   * allowed to pass this {@code EscapeTye} any of the methods in the {@link Renderer} class.
+   * allowed to pass this {@code EscapeType} any of the methods in the {@link Renderer} class.
    * However, if a template variable's escape type is {@code NOT_SPECIFIED}, its value will be
    * escaped using whatever {@code EscapeTye} <i>is</i> passed to these methods.
    */
-  NOT_SPECIFIED(
-      null,
-      s -> {
-        throw new UnsupportedOperationException();
-      }),
+  NOT_SPECIFIED(null, Check.fail("Invalid escape type: NOT_SPECIFIED")),
 
   /**
    * Do not apply any escaping. This is (most likely) the {@code EscapeType} to use if a template
@@ -32,7 +30,7 @@ public enum EscapeType {
    * the HTML tags themselves. This escape type can be specified within the template variable itself
    * using the {@code text} prefix. For example: {@code ~%text:tableRows%}
    */
-  ESCAPE_NONE("text", s -> s),
+  ESCAPE_NONE("text", UnaryOperator.identity()),
   /**
    * The type of escaping to be used for template variables inserted into HTML tags. For example:
    *
@@ -74,7 +72,25 @@ public enum EscapeType {
    * &lt;/script&gt;
    * </pre>
    */
-  ESCAPE_JS("js", StringEscapeUtils::escapeEcmaScript);
+  ESCAPE_JS("js", StringEscapeUtils::escapeEcmaScript),
+
+  /**
+   * The type of escaping to use if a template variable is embedded as a path segment within a URL.
+   * Note that it does not matter whether the URL as a whole is the value of a JavaScript variable
+   * or the contents of an HTML tag. The result of applying this escape type does not change when
+   * subsequently applying JavaScript or HTML escaping.
+   */
+  ESCAPE_URL_PATH_SEGMENT("ps", EscapeType::escapeUrlPathSegment),
+
+  /**
+   * The type of escaping to use if a template variable is embedded within a URL as a the value of
+   * URL query parameter. It can also be used in the more unlikely case that the variable functions
+   * as the <i>name</i> of the query parameter, because names and values are escaped in the same way
+   * in a URL. Note that it does not matter whether the URL as a whole is the value of a JavaScript
+   * variable or the contents of an HTML tag. The result of applying this escape type does not
+   * change when subsequently applying JavaScript or HTML escaping.
+   */
+  ESCAPE_URL_QUERY_PARAM("qp", EscapeType::escapeUrlQueryParam);
 
   /**
    * Parses the specified string into an {@code EscapeType}. Valid values are "text", "html" and
@@ -96,8 +112,12 @@ public enum EscapeType {
         return ESCAPE_HTML;
       case "js":
         return ESCAPE_JS;
+      case "ps":
+        return ESCAPE_URL_PATH_SEGMENT;
+      case "qp":
+        return ESCAPE_URL_QUERY_PARAM;
     }
-    throw new IllegalArgumentException("No such escape type: \"" + s + "\"");
+    return Check.fail("No such escape type: \"%s\"", s);
   }
 
   private final String prefix;
@@ -124,5 +144,15 @@ public enum EscapeType {
    */
   public String apply(String raw) {
     return escaper.apply(raw);
+  }
+
+  private static String escapeUrlPathSegment(String s) {
+    // Chop off the forward slash
+    return new URIBuilder().setPathSegments(s).toString().substring(1);
+  }
+
+  private static String escapeUrlQueryParam(String s) {
+    // Chop off ?x=
+    return new URIBuilder().addParameter("x", s).toString().substring(3);
   }
 }
