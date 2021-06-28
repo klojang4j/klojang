c94ffdbf27f199fa38da1aa56b1c323eea13aece
Ayco Holleman <ayco.holleman@naturalis.nl>
2021-06-28 09:08:52 +0200

    ok

--- src/main/java/nl/naturalis/yokete/render/Page.java
+++ src/main/java/nl/naturalis/yokete/render/Page.java
@@ -3,58 +3,72 @@ package nl.naturalis.yokete.render;
 import java.util.List;
 import nl.naturalis.common.check.Check;
 import nl.naturalis.yokete.template.Template;
+import static nl.naturalis.yokete.render.TemplateStringifiers.SIMPLE_STRINGIFIER;
 
 /**
- * The {@code Page} class is a factory for {@link RenderSession render sessions}. Its main component
- * is the {@link Template} to be rendered. Besides that it contains an {@link AccessorFactory} that
+ * A {@code Page} is a factory for {@link RenderSession render sessions}. Its main component is the
+ * {@link Template} to be rendered. Besides that it contains an {@link AccessorFactory} that
  * provides {@link Accessor accessors} for the data fed into the template and a {@link
- * TemplateStringifiers} telling the {@code RenderSession} how to stringify values. The {@code Page}
- * class is not only a factory for render sessions, it also injects itself into the {@code
- * RenderSession} instances it creates, so they have access to each of these components.
+ * TemplateStringifiers} telling the {@code RenderSession} how to stringify values.
  *
  * <p>Note that the name <i>Page</i> is somewhat misleading because an HTML template need not be a
- * full-blown HTML page. It can also be an HTML snippet that you insert into other pages.
+ * full-blown HTML page. It can also be an HTML snippet that you include or {@link
+ * RenderSession#paste(String, Renderable) "paste"} into other pages. Or it can be something else
+ * completely.
  *
  * @author Ayco Holleman
  */
 public final class Page {
 
   /**
-   * Returns a {@code Page} that will produce {@link RenderSession} instances lacking an {@link
-   * Accessor} and using the {@link Stringifier#DEFAULT default stringifier} to stringify values.
+   * Returns a {@code Page} that will produce {@link RenderSession} instances that lack an {@link
+   * Accessor}, and that use the {@link Stringifier#DEFAULT default stringifier} to stringify
+   * values.
    *
-   * @param template
-   * @return
+   * @param template The template to be populated
+   * @return A {@code Page} that will produce {@link RenderSession} instances for the specified
+   *     template
    */
   public static Page configure(Template template) {
-    return configure(template, TemplateStringifiers.SIMPLE_STRINGIFIER);
+    return configure(template, SIMPLE_STRINGIFIER);
   }
 
   /**
    * Returns a {@code Page} that will produce {@link RenderSession} instances lacking an {@link
-   * Accessor}. That means you <i>cannot</i> call the {@link RenderSession#populate(String, Object,
-   * String...) populate} and {@link RenderSession#insert(Object, EscapeType, String...) insert}
-   * methods of {@code RenderSession} to populate the template. If you do, a {@link RenderException}
-   * is thrown.
+   * AccessorFactory}. That means you <i>cannot</i> call the {@link RenderSession#populate(String,
+   * Object, String...) populate} and {@link RenderSession#insert(Object, EscapeType, String...)
+   * insert} methods to populate the template. If you do, a {@link RenderException} is thrown. You
+   * can still use the {@link RenderSession#set(String, Object) set} methods.
    *
    * @param template
    * @param stringifiers
    * @return
    */
   public static Page configure(Template template, TemplateStringifiers stringifiers) {
-    Accessor<?> acc =
-        (x, y) -> {
-          throw RenderException.noAccessorProvided();
-        };
+    Accessor<?> acc = (x, y) -> Check.fail(RenderException::noAccessorProvided);
     AccessorFactory af = (x, y) -> acc;
     return configure(template, af, stringifiers);
   }
 
   /**
    * Creates a {@code Page} that will produce {@link RenderSession render sessions} for the
-   * specified template, using the specified {@code specified Accessor} to extract values from the
-   * provided source data, and using the specified {@code TemplateStringifiers} to obtain
-   * stringifiers that can stringify those values.
+   * specified template, using the specified {@code specified AccessorFactory} to produce {@link
+   * Accessor accessors} for source data for the template, and using the {@link
+   * TemplateStringifiers#SIMPLE_STRINGIFIER default stringifier} to stringify the source data.
+   *
+   * @param template
+   * @return
+   */
+  public static Page configure(Template template, AccessorFactory accessor) {
+    return configure(template, accessor, SIMPLE_STRINGIFIER);
+  }
+
+  /**
+   * Creates a {@code Page} that will produce {@link RenderSession render sessions} for the
+   * specified template, using the specified {@code specified AccessorFactory} to produce {@link
+   * Accessor accessors} for the source data for the template, and using the specified {@code
+   * TemplateStringifiers} to obtain stringifiers to stringify the values retrieved by the
+   * accessors.
    *
    * @param template The template for which the {@code Page} will create render sessions
    * @param accessor The {@code Accessor} implementation to use for extracting values from source
