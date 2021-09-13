2924b5c8fe9bcf3a75095ebd5dfa6da9b5c47181
Ayco Holleman <ayco.holleman@naturalis.nl>
2021-09-13 19:49:28 +0200

    ok

--- src/main/java/org/klojang/x/db/rs/ExtractorNegotiator.java
+++ src/main/java/org/klojang/x/db/rs/ExtractorNegotiator.java
@@ -2,7 +2,6 @@ package org.klojang.x.db.rs;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.Map;
 import nl.naturalis.common.ModulePrivate;
 import nl.naturalis.common.check.Check;
@@ -18,6 +17,7 @@ import static nl.naturalis.common.ClassMethods.className;
  * combines a {@code getXXX} method with an (optional) converter function.
  */
 @ModulePrivate
+@SuppressWarnings("rawtypes")
 public class ExtractorNegotiator {
 
   private static ExtractorNegotiator INSTANCE;
@@ -36,13 +36,12 @@ public class ExtractorNegotiator {
    */
   static final Integer DEFAULT = Integer.MIN_VALUE;
 
-  private final TypeMap<ExtractorLookup<?>> extractorsByType;
+  private final TypeMap<Map<Integer, RsExtractor>> extractorsByType;
 
   private ExtractorNegotiator() {
     extractorsByType = configure();
   }
 
-  @SuppressWarnings("unchecked")
   public <T, U> RsExtractor<T, U> findExtractor(Class<U> fieldType, int sqlType) {
     if (!extractorsByType.containsKey(fieldType)) {
       return Check.fail("Type not supported: %s", className(fieldType));
@@ -50,56 +49,34 @@ public class ExtractorNegotiator {
     // This implicitly checks that the specified int is one of the
     // static final int constants in the java.sql.Types class
     String sqlTypeName = sqlType == DEFAULT ? null : getTypeName(sqlType);
-    Map<Integer, RsExtractor<?, ?>> extractors = extractorsByType.get(fieldType);
+    Map<Integer, RsExtractor> extractors = extractorsByType.get(fieldType);
     if (!extractors.containsKey(sqlType)) {
       if (extractors.containsKey(DEFAULT)) {
-        return (RsExtractor<T, U>) extractors.get(DEFAULT);
+        return extractors.get(DEFAULT);
       }
       return Check.fail("Cannot convert %s to %s", sqlTypeName, className(fieldType));
     }
-    return (RsExtractor<T, U>) extractors.get(sqlType);
+    return extractors.get(sqlType);
   }
 
-  private static TypeMap<ExtractorLookup<?>> configure() {
-    HashMap<Class<?>, ExtractorLookup<?>> map = new HashMap<>();
-    ExtractorLookup<?> extractors;
-
-    extractors = my(new IntExtractors());
-    map.put(Integer.class, extractors);
-    map.put(int.class, extractors);
-
-    extractors = my(new LongExtractors());
-    map.put(Long.class, extractors);
-    map.put(long.class, extractors);
-
-    extractors = my(new DoubleExtractors());
-    map.put(Double.class, extractors);
-    map.put(double.class, extractors);
-
-    extractors = my(new FloatExtractors());
-    map.put(Float.class, extractors);
-    map.put(float.class, extractors);
-
-    extractors = my(new ShortExtractors());
-    map.put(Short.class, extractors);
-    map.put(short.class, extractors);
-
-    extractors = my(new ByteExtractors());
-    map.put(Byte.class, extractors);
-    map.put(byte.class, extractors);
-
-    extractors = my(new BooleanExtractors());
-    map.put(Boolean.class, extractors);
-    map.put(boolean.class, extractors);
-
-    map.put(String.class, my(new StringExtractors()));
-    map.put(LocalDate.class, my(new LocalDateExtractors()));
-    map.put(LocalDateTime.class, my(new LocalDateTimeExtractors()));
-    map.put(Enum.class, my(new EnumExtractors()));
-    return TypeMap.withTypes(map);
+  private static TypeMap<Map<Integer, RsExtractor>> configure() {
+    return TypeMap.build(Map.class)
+        .autobox()
+        .add(String.class, my(new StringExtractors()))
+        .add(int[].class, my(new IntExtractors()))
+        .add(double.class, my(new DoubleExtractors()))
+        .add(long.class, my(new LongExtractors()))
+        .add(float.class, my(new FloatExtractors()))
+        .add(short.class, my(new ShortExtractors()))
+        .add(byte.class, my(new ByteExtractors()))
+        .add(boolean.class, my(new BooleanExtractors()))
+        .add(LocalDate.class, my(new LocalDateExtractors()))
+        .add(LocalDateTime.class, my(new LocalDateTimeExtractors()))
+        .add(Enum.class, my(new EnumExtractors()))
+        .freeze();
   }
 
-  private static ExtractorLookup<?> my(ExtractorLookup<?> src) {
-    return src.copy();
+  private static Map<Integer, RsExtractor> my(ExtractorLookup<?> src) {
+    return Map.copyOf(src);
   }
 }
