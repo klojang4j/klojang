d7380c41253d2e4af6b1daa441e1417df7ee022a
Ayco Holleman <ayco.holleman@naturalis.nl>
2021-10-08 17:20:39 +0200

    ok

--- src/main/java/org/klojang/util/RequestBeanifier.java
+++ src/main/java/org/klojang/util/RequestBeanifier.java
@@ -5,9 +5,7 @@ import nl.naturalis.common.invoke.BeanWriter;
 
 /**
  * Utility class that lets you quickly convert a request parameter map (typically a JAX-RS {@code
- * MultiValuedMap} or a {@code Map<String, String[]>}) to a JavaBean. This class is more versatile
- * than using the <code>@BeanParam
- * </code> mechanism because it will also work with {@code multipart/form-data} requests.
+ * MultiValuedMap} or a {@code Map<String, String[]>}) to a JavaBean.
  *
  * @author Ayco Holleman
  * @param <T> The type of the JavaBean
@@ -26,17 +24,17 @@ public class RequestBeanifier<T> {
   }
 
   /**
-   * Populates the specified bean with the values of the specified request parameters.
+   * Populates the specified bean with request parameter values.
    *
    * @param bean The bean to populate
-   * @param params The request parameters. Although you would typically provide a JAX-RS {@code
-   *     MultiValuedMap} or a {@code Map<String, String[]>}, this method really works with any type
-   *     of map values.
+   * @param params The request parameters. Although the provided map would typically be a JAX-RS
+   *     {@code MultiValuedMap} or a {@code Map<String, String[]>}, this method imposes no such
+   *     restrictions.
    * @return The populated bean
    * @throws Throwable An exception arising from the dynamic invocation of the bean's setters
    */
   public T beanify(T bean, Map<String, ?> params) throws Throwable {
-    writer.set(bean, params);
+    writer.copy(params, bean);
     return bean;
   }
 }
