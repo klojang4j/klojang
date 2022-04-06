a8ea82d480e6386c920855cb940006ea88ef5dca
Ayco Holleman <ayco.holleman@naturalis.nl>
2022-04-06 20:15:47 +0200

    sealed segmentwriter & segmentreader

--- src/main/java/org/klojang/x/acc/PathAccessor.java
+++ src/main/java/org/klojang/x/acc/PathAccessor.java
@@ -1,12 +1,14 @@
 package org.klojang.x.acc;
 
 import java.util.List;
+
 import org.klojang.template.Accessor;
 import org.klojang.template.NameMapper;
 import org.klojang.template.RenderException;
 import nl.naturalis.common.path.Path;
 import nl.naturalis.common.path.PathWalker;
-import static nl.naturalis.common.path.PathWalker.DeadEndAction.*;
+
+import static nl.naturalis.common.path.PathWalker.OnDeadEnd.*;
 
 public class PathAccessor implements Accessor<Object> {
 
@@ -18,8 +20,13 @@ public class PathAccessor implements Accessor<Object> {
 
   @Override
   public Object access(Object data, String property) throws RenderException {
-    String path = nm == null ? property : nm.map(property);
-    Object val = new PathWalker(List.of(new Path(path)), RETURN_DEAD_END).read(data);
-    return val == PathWalker.DEAD_END ? UNDEFINED : val;
+    String path = nm == null
+        ? property
+        : nm.map(property);
+    Object val = new PathWalker(List.of(new Path(path)), RETURN_DEAD).read(data);
+    return val == PathWalker.DEAD
+        ? UNDEFINED
+        : val;
   }
+
 }
